8d553243d093a637d3a87b54e03c198804be4cd5
Pablo Ventura <pablogventura@gmail.com>
2019-04-25 21:22:34 -0300

    creo que ya esta

--- results_megahit.py
+++ results_megahit.py
@@ -74,11 +74,11 @@ ax.set_ylabel('Time ($s$)')
 ax.set_title('Time to decide definability')
 
 #plt.semilogx(2)
-# plt.loglog(basex=2)
-plt.semilogx(basex=2)
+plt.loglog(basex=2,basey=2)
+#plt.semilogx(basex=2)
 print(new_new_results)
 for k in new_new_results:
-    plt.plot([8,16, 32, 64], [i[1] for i in new_new_results[k]], label=k)
+    plt.plot([8,16, 32,64], [i[1] for i in new_new_results[k]], label=k)
 
 leg = plt.legend(loc='best', ncol=1, shadow=True, fancybox=True)
 leg.get_frame().set_alpha(0.5)
